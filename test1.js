335931011f3805883aa61cae151c72c0ca3e49f4
abhishek <abhi4321c2@gmail.com>
2022-01-17 02:16:38 +0530

    initial commit

--- test1.js
+++ test1.js
@@ -1,3 +1,10 @@
 const fs = require("fs");
 const text = fs.readFileSync("del.txt", "utf-8");
+
+text = text.replace("content", "rohan");
+
+console.log("the contennt is");
 console.log(text);
+
+console.log("creating a new file");
+fs.writeFileSync("rohan.txt", text);
